99a3581ea1c3c36267cde46a12762d6d0f1b12fb
Nichole <nicholewilburn@gmail.com>
2023-07-31 17:32:30 -0500

    fixes-and-features-01

---
+++ db/seeds.sql
@@ -0,0 +1,57 @@
+USE business_db;
+
+INSERT INTO department (dept_name)
+VALUES (
+    "Front of House"
+),
+(
+    "Back of House"
+);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES (
+    "FoH Manager",
+    "200",
+    "1"
+),
+(
+    "Waiter",
+    "100",
+    "1"
+),
+(
+    "BoH Manager",
+    "200",
+    "2"
+),
+(
+    "Cook",
+    "100",
+    "2"
+);
+
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+VALUES (
+    "BigMan",
+    "Upstairs",
+    "1",
+    "1"
+),
+(
+    "Julia",
+    "TakesYourOrder",
+    "2",
+    "1"
+),
+(
+    "Gordon",
+    "Ramsey",
+    "3",
+    "3"
+),
+(
+    "Idiot",
+    "Sandwich",
+    "4",
+    "3"
+);
